840abe0ecc77ea3e23b5bede8cca96d157e03374
andre <andrefrateschi@gmail.com>
2020-09-02 19:03:30 -0300

    commit 01

---
+++ tabuadasimples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tabuadasimples
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            double numero;
+            Console.WriteLine("digite o numero");
+
+            numero = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 9999999; i++)
+            {
+                double total = numero * i;
+                Console.WriteLine(numero + " X " + i + " = " + total);
+
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
